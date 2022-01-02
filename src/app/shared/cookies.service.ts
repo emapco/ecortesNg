e629a48a46d2886dab280231524ec571b394fd5e
Emmanuel Cortes <polosetroc@gmail.com>
2022-01-02 00:02:23 -0800

    Added cookies to store the user's current display mode.

---
+++ src/app/shared/cookies.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookiesService {
+
+  constructor(private cookie: CookieService) { }
+
+  setMode(isLightMode: boolean): void {
+    this.cookie.set('isLightMode', String(isLightMode));
+  }
+
+  getMode(): boolean {
+    return (this.cookie.get('isLightMode') === 'true');
+  }
+}
