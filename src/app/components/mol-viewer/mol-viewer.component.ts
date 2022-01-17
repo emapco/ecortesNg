96aae1d8327755490ffd39213dca799172e03faf
Emmanuel <polosetroc@gmail.com>
2022-01-16 21:04:10 -0800

    Refactored mol viewer component and small content change to pathfinding visualizer

--- src/app/components/mol-viewer/mol-viewer.component.ts
+++ src/app/components/mol-viewer/mol-viewer.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject, OnChanges, OnInit} from '@angular/core';
+import {Component, Inject, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import * as $ from 'jquery';
 // @ts-ignore
 import * as $3Dmol from '3dmol';
@@ -21,103 +21,36 @@ export class MolViewerComponent implements OnInit {
 
   constructor(
     private molData: MolDataService,
-    @Inject(MAT_DIALOG_DATA) public data: { folder: string, file: string }) {}
+    @Inject(MAT_DIALOG_DATA) public data: { folder: string, file: string }) {
+  }
 
   async ngOnInit() {
     await this.init3Dmol(this.data.folder, this.data.file);
-    // change innerWidth otherwise webGL canvas width is indeterminate
-    $('#container').innerWidth(352);
   }
 
+  /**
+   * Uses 3DMolJS library to display mol2 structure data
+   * @param folder - folder to look for mol2 file
+   * @param file - mol2 file name to be displayed
+   */
   async init3Dmol(folder: string, file: string): Promise<void> {
+    let displayElement = $('#container');
     // get mol data from file
     let mol2Data = await this.molData.get(folder, file);
-
     let config = {backgroundAlpha: 0};
-    let viewer = $3Dmol.createViewer("container", config);
+    // create viewer on displayElement element and apply config style
+    let viewer = $3Dmol.createViewer(displayElement, config);
+    // add data to viewer
     viewer.addModel(mol2Data, "mol2");
+    // customize the viewer
     viewer.setStyle({
       sphere: {scale: 1, opacity: 1, hidden: false, radius: .3},
       stick: {opacity: 1, radius: .1},
     });
-    viewer.zoomTo();
-    viewer.resize();
-    viewer.render();
-    //await this.initMol(viewer, mol2DataValue);
-  }
-
-  async initMol(viewer: any, mol2Data: String): Promise<void> {
-    viewer.addModel(mol2Data, "mol2");
-    /*
-    let atomLabels = this.getAtomLabels(mol2Data);
-    let i;
-    for (i = 0; i < atomLabels.length; i++) {
-      let atomWithLabel = atomLabels[i];
-      viewer.addLabel(atomWithLabel.label, {
-        position: atomWithLabel.atom.coord,
-        backgroundColor: 0xff0000,
-        backgroundOpacity: 0.8
-      });
-    }
-     */
-    viewer.setStyle({
-      sphere: {scale: 1, opacity: 1, hidden: false, radius: .3},
-      stick: {opacity: 1, radius: .1},
-    });
+    // zoom in on molecule and display it
     viewer.zoomTo();
     viewer.render();
-    viewer.resize();
-  };
-
-  getAtomLabels(sdfData: String): Array<AtomLabel> {
-    let atoms = [];
-    let lines = sdfData.split(/\r?\n|\r/);
-    let atomCount = parseInt(lines[3].slice(0, 3));
-    let bondCount = parseInt(lines[3].slice(3, 3));
-
-    let i, line;
-    for (i = 4; i < 4 + atomCount; i++) {
-      line = lines[i];
-      let elem = line.slice(31, 3).replace(/ /g, "");
-
-      let atom = new Atom(
-        elem[0].toUpperCase() + elem.slice(1).toLowerCase(),
-        new Coordinate(
-          parseFloat(line.slice(0, 10)),
-          parseFloat(line.slice(10, 10)),
-          parseFloat(line.slice(20, 10))
-        )
-      );
-      atoms.push(atom);
-    }
-
-    let atomLabels = [];
-    for (i = 4 + atomCount + bondCount; i < lines.length; i++) {
-      line = lines[i];
-      if (line.startsWith("A")) {
-        let parts = line.split(/\s+/);
-        let atomSerial = parseInt(parts[1]);
-        let atomWithLabel = new AtomLabel(lines[i + 1], atoms[atomSerial - 1]);
-        i++;
-        atomLabels.push(atomWithLabel);
-      }
-    }
-    return atomLabels;
-  };
-
-}
-
-class Coordinate {
-  constructor(public x: number, public y: number, public z: number) {
-  }
-}
-
-class Atom {
-  constructor(public atom: String, public coord: Coordinate) {
-  }
-}
-
-class AtomLabel {
-  constructor(public label: String, public atom: Atom) {
+    // set innerWidth otherwise WebGL canvas width is indeterminate
+    displayElement.innerWidth(352);
   }
 }
